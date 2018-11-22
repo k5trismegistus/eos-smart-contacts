838af5c2c68ae7723cf6f2c3c91e9273f9bcf803
keigo <k5.trismegistus@gmail.com>
2018-11-23 00:20:57 +0900

    :sparkles: soukin

---
+++ contracts/soukin/deploy.sh
@@ -0,0 +1,3 @@
+eosiocpp -o /mycontracts/soukin/soukin.wast /mycontracts/soukin/soukin.cpp
+eosiocpp -g /mycontracts/soukin/soukin.abi /mycontracts/soukin/soukin.cpp
+cleos -u https://api-kylin.eoslaomao.com set contract kei5yamamoto /mycontracts/soukin
